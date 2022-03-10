cd4aff4a790f3ee794691dd7d236b88db6e3768f
Fatima1899 <99797429+Fatima1899@users.noreply.github.com>
2022-03-11 01:30:56 +0400

    Add files via upload

---
+++ Book.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Class_Inheritance.Models
+{
+    internal class Book : Product
+    {
+        public string authorName;
+        public int pagecount;
+        public double discountPercent;
+
+        public void GetInfo()
+        {
+            Console.WriteLine($"{name}{authorName}{pagecount}{discountPercent}{costPrice}");
+        }
+        public double GetDiscountedPrice()
+        {
+            return salePrice - ((salePrice * discountPercent) / 100);
+        }
+    }
+}
---
+++ Person.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassTask.Models
+{
+    internal class Person
+    {
+        public string name;
+        public string surname;
+        public int age;
+
+        public Person(string name, string surname)
+        {
+            this.name = name;
+            this.surname = surname;
+        }
+        public void Fullname()
+        {
+            Console.WriteLine($"{name} {surname}");
+        }
+        public int GetAge()
+        {
+            return age;
+        }
+    }
+}
